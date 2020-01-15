d6205ebf81602c18af66b5da63ea49781916a1d0
Hans van Meurs <hans@androidappfactory.nl>
2020-01-15 13:30:11 +0100

    Initial Commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `
+  templateUrl: './servers.component.html'
+})
+export class ServersComponent implements OnInit {
+
+  allowAddNewServer = false;
+  serverCreationStatus = "Not yet created";
+  serverName = '';
+
+  constructor() { 
+
+    setTimeout( () => {
+      this.allowAddNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onServerCreation(){
+    this.serverCreationStatus = "The server is created succesfully: " + this.serverName; 
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement> event.target).value;
+  }
+}
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-waringalert',
+    templateUrl: './warningalert.component.html'
+})
+export class WarningalertComponent {
+
+}
